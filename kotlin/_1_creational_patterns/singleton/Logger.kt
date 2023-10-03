2466f5ee5c68e5bc2e09f5be27f6c8c37c473289
Bangseungjae <tmdwo5825@naver.com>
2023-10-03 18:38:00 +0900

    change singleton file

--- kotlin/_1_creational_patterns/singleton/Logger.kt
+++
@@ -1,9 +0,0 @@
-package _1_creational_patterns.singleton
-
-object Logger {
-    val name = "Joy"
-    var age = 10
-    init {
-        println("${name}가 싱글톤 객체에 처음 접근했습니다. age = ${age++}")
-    }
-}

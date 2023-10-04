332108b33d2615bc6e57305519940918e25dac9d
Bangseungjae <tmdwo5825@naver.com>
2023-10-04 14:14:50 +0900

    fix

--- kotlin/_1_creational_patterns/_2_factorymethod/FactoryMethod.kt
+++ kotlin/_1_creational_patterns/_2_factorymethod/FactoryMethod.kt
@@ -1,2 +1,32 @@
 package _1_creational_patterns._2_factorymethod
 
+fun main() {
+    val queen = createPiece("qa5")
+    println(queen)
+}
+
+interface ChessPiece {
+    val file: Char
+    val rank: Char
+}
+
+data class Pawn(
+    override val file: Char,
+    override val rank: Char
+) : ChessPiece
+
+
+data class Queen(
+    override val file: Char,
+    override val rank: Char
+) : ChessPiece
+
+fun createPiece(notation: String): ChessPiece {
+    val (type, file, rank) = notation.toCharArray()
+    return when (type) {
+        'q' -> Queen(file, rank)
+        'p' -> Pawn(file, rank)
+        // ...
+        else -> throw RuntimeException("Unknown piece: $type")
+    }
+}
